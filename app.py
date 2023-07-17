53afe9726a11e533a95b27b30a92e32ad34e51e5
HEquatos <139774415+HEquatos@users.noreply.github.com>
2023-07-17 14:52:14 +0200

    Add files via upload

---
+++ app.py
@@ -0,0 +1,27 @@
+import streamlit as st
+import pandas as pd
+
+def main():
+    st.title('Taux de Bons du Trésor')
+
+    # Champ d'entrée pour l'AMC
+    amc = st.text_input('AMC (Acteur du Marché Central)')
+
+    # Champ d'entrée pour la date de valorisation
+    dv = st.date_input('Date de valorisation')
+
+    # Champ d'entrée pour la date de maturation
+    dc = st.date_input('Date de maturation (DC)')
+
+    
+    # Bouton pour valider les modifications
+    if st.button('Valider'):
+        # Ajoutez ici la logique pour traiter les données saisies et effectuer les calculs nécessaires
+
+        # Exemple : affichage des données saisies
+        st.write('AMC:', amc)
+        st.write('Date de valorisation:', dv)
+        st.write('Date de maturation:', dc)
+
+if __name__ == '__main__':
+    main()
