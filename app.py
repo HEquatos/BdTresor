096b9d102b9f233bebb8b394c7c62d5ebc21b87e
HEquatos <139774415+HEquatos@users.noreply.github.com>
2024-10-12 23:25:04 +0200

    Update app.py

--- app.py
+++ app.py
@@ -92,7 +92,7 @@ def get_courbe_data(date):
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
     }
     
-    response = requests.get(url, headers=headers)
+    response = requests.get(url, headers=headers, verify=False)
 
     # Check if the response is successful
     if response.status_code == 200:
